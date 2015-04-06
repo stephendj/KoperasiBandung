aeb74784e6d8703cb69f9bfa0176159b26ceea02
stephendj <stephendj181194@gmail.com>
2015-04-06 13:36:07 +0700

    First commit, design only

---
+++ app/routes.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+Route::group(array('prefix' => '/'), function()
+{
+	Route::get('/', function() {
+        return View::make('daftar-koperasi');
+    });
+    Route::get('tanya', function() {
+    	return View::make('FAQ');
+    });
+    Route::get('ajukan-bentuk', function() {
+    	return View::make('pengajuan-pembentukan');
+    });
+    Route::get('ajukan-bubar', function() {
+    	return View::make('pengajuan-pembubaran');
+    });
+    Route::get('audit', function() {
+    	return View::make('unggah-audit');
+    });
+});
+
+Route::group(array('prefix' => 'admin'), function() {
+    Route::get('/', function() {
+    	return View::make('login');
+    });
+    Route::post('/', function() {
+    	return Redirect::to('admin/ajuan');
+    });
+    Route::get('ajuan', function() {
+    	return View::make('manajemen-ajuan');
+    });
+    Route::get('koperasi', function() {
+    	return View::make('manajemen-koperasi');
+    });
+    Route::get('jawab', function() {
+    	return View::make('jawab-pertanyaan');
+    });
+    Route::get('audit', function() {
+    	return View::make('cek-audit');
+    });
+});

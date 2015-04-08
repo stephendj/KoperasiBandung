72a3f07d78e8a7218df1442b1fb95fb9fbf804c8
ivanaclairineirsan <ivana.clairine@gmail.com>
2015-04-08 23:57:51 +0700

    syarat pendirian

--- app/views/pengajuan-pembentukan.blade.php
+++ app/views/pengajuan-pembentukan.blade.php
@@ -7,14 +7,28 @@
 				<div class="col-md-4">
 				<div class="company_ad">
 				     	<h2>Syarat Pengajuan Pembentukan Koperasi:</h2>
-      				
-						 <p>1. Bla bla bla bla</p>
-						 <p>2. Bla bla bla bla</p>
-						 <p>3. Bla bla bla bla</p>
-						 <p>4. Bla bla bla bla</p>
-						 <p>5. Bla bla bla bla</p>
-						 <p>6. Bla bla bla bla</p>
-						 <p>7. Bla bla bla bla</p>
+      					 <p> Untuk Koperasi selain Koperasi Simpan Pinjam  : </p>
+						 <p>1. Dua rangkap Salinan Akta Pendirian koperasi dari notaris (NPAK).</p>
+						 <p>2. Berita Acara Rapat Pendirian Koperasi.</p>
+						 <p>3. Daftar hadir rapat pendirian koperasi</p>
+						 <p>4. Foto Copy KTP Pendiri (urutannya disesuaikan dengan daftar hadir agar mempermudah pd saat verifikasi).</p>
+						 <p>5. Kuasa pendiri (Pengurus terpilih) untuk mengurus pengesahan pembentukan koperasi.</p>
+						 <p>6. Surat Bukti tersedianya modal yang jumlahnya sekurang;kurangnya sebesar simpanan pokok dan simpanan wajib yang wajib dilunasi para pendiri.</p>
+						 <p>7. Rencana kegiatan usaha koperasi minimal tiga tahun kedepan dan Rencana Anggaran Belanja dan Pendapatan Koperasi.</p>
+						 <p>8. Daftar susunan pengurus dan pengawas.</p>
+						 <p>9. Daftar Sarana Kerja Koperasi.</p>
+						 <p>10. Surat pernyataan tidak mempunyai hubungan keluarga antara pengurus.</p>
+						 <p>11. Struktur Organisasi Koperasi. </p>
+						 <p>12. Surat Pernyataan Status kantor koperasi dan bukti pendukungnya</p>
+						 <p>13. Dokumen lain yang diperlukan sesuai dengan peraturan perundang-undangan</p>
+						 <p>Tambahan syarat untuk pendirian Koperasi Simpan Pinjam : </p>
+						 <p>1. Surat bukti penyetoran modal sendiri pada awal pendirian, itu berupa deposito pada bank pemerintah atas nama menteri negara koperasi dan umkm. </p>
+						 <p>2. Kelengkapan administrasi organisasi dan pembukuan usp yang dikelola secara kusus dan terpisah dari pembukuan koperasinya.</p>
+						 <p>3. Nama dan riwayat hidup pengurus dan pengawas </p>
+						 <p>4. Surat perjanjian kerja antara pengurus koperasi dengan pengelola USP koperasi</p>
+						 <p>5. Nama dan riwayat hidup calon pengelola yang dilengkapi dengan beberapa poin berikut seperti bukti telah mengikuti pelatihan atau magang usaha simpan pinjam koperasi, surat keterangan berkelakuan baik atau SKCK, surat pernyataan tidak mempunyai hubungan sedarah dengan pengurus dan pengawas, dan terakhir adalah surat pernyataan pengelola tentang kesediaannya untuk bekerja secara purna waktu. </p>
+						 <p>6. Permohonan ijin menyelenggarakan usaha simpan pinjam </p>
+						 <p>7. Menyediakan surat pernyataan bersedia untuk diperiksa dan dinilai kesehatan USP koperasinya oleh pejabat yang berwenang. Info lebih detail, dapat anda lihat di situs Kementerian Negara Koperasi dan UKM.</p>
 				   	
 			   	</div>
 				</div>				

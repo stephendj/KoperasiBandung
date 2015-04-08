43b081c335ec4eb1a1d2a103759f563356adc638
ivanaclairineirsan <ivana.clairine@gmail.com>
2015-04-08 23:20:12 +0700

    pertanyaan CRD

---
+++ app/controllers/PertanyaanController.php
@@ -0,0 +1,33 @@
+<?php
+
+class PertanyaanController extends BaseController {
+
+	public function showPertanyaan()
+	{
+		$pertanyaan=Pertanyaan::showPertanyaan();
+		return View::make('daftar-pertanyaan')->with('pertanyaan',$pertanyaan);
+	}
+
+	
+	public function addPertanyaan()
+	{
+		$pertanyaan_user=Input::get('userMsg');
+		$nama=Input::get('nama');
+		$email=Input::get('email');
+
+		Pertanyaan::createPertanyaan($pertanyaan_user, $nama, $email);
+		return Redirect::back()->with('message','Pertanyaan telah disimpan');;
+	}
+
+	public function addJawaban($id1) //edit Pertanyaan
+	{
+		
+	}
+
+	public function deletePertanyaan($id)
+	{
+		Pertanyaan::deletePertanyaan($id);
+		return Redirect::back();
+	}
+
+}
